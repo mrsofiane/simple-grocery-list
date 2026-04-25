612cecbd1e329e02f2b4a78c570a2ff99b5355d8
Sofiane Louchene <sofianelilou@gmail.com>
2026-04-25 22:19:18 +0200

    chore: bump version to 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "me.mrsofiane.simplegrocerylist"
         minSdk = 21
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.1"
+        versionCode = 5
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
