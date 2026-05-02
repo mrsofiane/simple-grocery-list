8682e579ac745c4db0110624ebff0141a0270536
Sofiane Louchene <sofianelilou@gmail.com>
2026-05-02 19:42:57 +0200

    chore: upgrade Gradle 9.5, JDK 17, BOM 2026.04, drop unnecessary @OptIn

--- app/src/main/java/me/mrsofiane/simplegrocerylist/ui/components/GroceryRow.kt
+++ app/src/main/java/me/mrsofiane/simplegrocerylist/ui/components/GroceryRow.kt
@@ -21,7 +21,6 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -43,7 +42,6 @@ import me.mrsofiane.simplegrocerylist.model.getCategoryColor
 import me.mrsofiane.simplegrocerylist.model.getCategoryIcon
 import me.mrsofiane.simplegrocerylist.ui.theme.DeleteRed
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SwipeableGroceryRow(
     item: GroceryItem,
